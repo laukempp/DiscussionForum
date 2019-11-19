26dd1f04f037288bd4a3d03e27a3fac90dd57d92
heieidi <heidi.p.hennessy@gmail.com>
2019-11-19 15:07:09 +0200

    Tietokantaan korjattu comment table ja foreign_key. Testit menee myös läpi KOKO CRUDille

--- database.sql
+++ database.sql
@@ -1,18 +1,22 @@
 -- CREATE DATABASE forum;
 
 -- \c forum;
-
+CREATE TABLE topic(
+id SERIAL PRIMARY KEY,
+nickname VARCHAR(255) NOT NULL,
+title VARCHAR(255),
+posttime TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 CREATE TABLE comment(
     id SERIAL PRIMARY KEY,
-    title text,
-    input VARCHAR(5000),
-    UNIQUE(title)
+    input text,
+    topic_id int REFERENCES topic(id),
+    img bytea,
+    c_nickname varchar(255) NOT NULL,
+    c_posttime TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-CREATE TABLE topic(
-    topic_id SERIAL PRIMARY KEY,
-    nickname VARCHAR(255) NOT NULL,
-    title VARCHAR(255) REFERENCES comment(title) NOT NULL,
-    posttime TIMESTAMP);
 
 
-/* INSERT INTO topic(nickname, title, comment) VALUES('testinimi', 'testiotsikko', 'testitestitesti'); */
+
+/* INSERT INTO topic(nickname, title) VALUES('testinimi', 'testiotsikko');
+ */
--- forum_server/service/request.service.js
+++ forum_server/service/request.service.js
@@ -30,8 +30,8 @@ function getSingleTopic(req, callback) {
 function createTopic(req, callback) {
     pool.connect((err, client) => {
         if (err) throw err;
-        client.query('INSERT INTO topic(nickname, title, comment) VALUES($1, $2, $3)',
-                [req.body.nickname, req.body.title, req.body.comment], (err, data) => {
+        client.query('INSERT INTO topic(nickname, title) VALUES($1, $2)',
+                [req.body.nickname, req.body.title], (err, data) => {
             if (err) throw err;
             client.release();
             callback();
@@ -60,8 +60,8 @@ function removeTopic(req, res, callback) {
 function updateTopic(req, res, callback) {
     pool.connect((err, client) => {
         if (err) throw err;
-        client.query('UPDATE topic SET nickname = $1, title = $2, comment = $3 WHERE id = $4',
-            [req.body.nickname, req.body.title, req.body.comment, parseInt(req.params.id)], (err, data) => {
+        client.query('UPDATE topic SET nickname = $1, title = $2 WHERE id = $3',
+            [req.body.nickname, req.body.title, parseInt(req.params.id)], (err, data) => {
                 if (err) throw err;
                 client.release();
                 res.json(req.body)
