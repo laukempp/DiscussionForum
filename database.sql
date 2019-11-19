b441e41e0450468dcb475e32df5275dd2d4bdc26
heieidi <heidi.p.hennessy@gmail.com>
2019-11-19 14:13:06 +0200

    TABLE comment luotu ja title asetettu foreign keyksi

--- database.sql
+++ database.sql
@@ -1,12 +1,18 @@
-CREATE DATABASE forum;
+-- CREATE DATABASE forum;
 
-\c forum;
+-- \c forum;
 
-CREATE TABLE topic(
+CREATE TABLE comment(
     id SERIAL PRIMARY KEY,
+    title text,
+    input VARCHAR(5000),
+    UNIQUE(title)
+);
+CREATE TABLE topic(
+    topic_id SERIAL PRIMARY KEY,
     nickname VARCHAR(255) NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    comment VARCHAR(5000),
+    title VARCHAR(255) REFERENCES comment(title) NOT NULL,
     posttime TIMESTAMP);
 
-INSERT INTO topic(nickname, title, comment) VALUES('testinimi', 'testiotsikko', 'testitestitesti');
+
+/* INSERT INTO topic(nickname, title, comment) VALUES('testinimi', 'testiotsikko', 'testitestitesti'); */
