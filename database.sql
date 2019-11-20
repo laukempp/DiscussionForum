93aaf23b6d759a926902ef709f5f4844a7c503da
sormunen <sormunen.roni@gmail.com>
2019-11-20 16:13:42 +0200

    database alustus topicit tehty sql fileen

--- database.sql
+++ database.sql
@@ -19,5 +19,12 @@ CREATE TABLE comment(
 
 
 
-/* INSERT INTO topic(nickname, title) VALUES('testinimi', 'testiotsikko');
- */
+ INSERT INTO topic(nickname, title, input, posttime) VALUES('Kari', 'testiotsikko', 'alustuskommentti', DEFAULT);
+ INSERT INTO topic(nickname, title, input, posttime) VALUES('Mari', 'CSS-fanit', 'alustuskommentti', DEFAULT);
+ INSERT INTO topic(nickname, title, input, posttime) VALUES('Sari', 'jQuery', 'alustuskommentti', DEFAULT);
+ INSERT INTO topic(nickname, title, input, posttime) VALUES('Jari', 'react', 'alustuskommentti', DEFAULT);
+ INSERT INTO topic(nickname, title, input, posttime) VALUES('Harry', 'typescript', 'alustuskommentti', DEFAULT);
+ INSERT INTO topic(nickname, title, input, posttime) VALUES('Barry', 'angular', 'alustuskommentti', DEFAULT);
+ INSERT INTO topic(nickname, title, input, posttime) VALUES('Jerry', 'nodejs', 'alustuskommentti', DEFAULT);
+
+ 
